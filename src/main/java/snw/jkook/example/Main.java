e83cc16bf3624aa5ab7ad09b3c721a014d549e26
ZX夏夜之风 <snwcreations@qq.com>
2022-08-12 16:33:45 +0800

    Upgrade JKook API to 0.33.0

--- src/main/java/snw/jkook/example/Main.java
+++ src/main/java/snw/jkook/example/Main.java
@@ -1,16 +1,8 @@
 package snw.jkook.example;
 
-import org.slf4j.Logger;
 import snw.jkook.plugin.BasePlugin;
-import snw.jkook.plugin.PluginDescription;
-
-import java.io.File;
 
 public class Main extends BasePlugin {
-
-    protected Main(File configFile, File dataFolder, PluginDescription description, File file, Logger logger) {
-        super(configFile, dataFolder, description, file, logger);
-    }
 
     @Override
     public void onEnable() {
